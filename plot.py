9e82c74b0f068861304e3d4d86770912a3f2235d
David <david@bilbo.(none)>
2009-10-10 15:10:23 -0400

    Implemented convert_point so it passes the most basic test

--- plot.py
+++ plot.py
@@ -28,20 +28,27 @@ def plot(data,yrange=(0,200)):
         clock.tick(20)
 
 def convert_point(point, xrange, yrange, screen_range):
-    xfactor = screen_range[0] /
-    yfactor = screen_range[1]
+    """Convert the coordinates of a point based on the plotting range and the screen size"""
+    xfactor = screen_range[0] / float(xrange[1]-xrange[0])
+    yfactor = screen_range[1] / float(yrange[1]-yrange[0])
+    xoffset = -xrange[0] * xfactor
+    yoffset = -yrange[0] * yfactor
+
+    x =  point[0] * xfactor + xoffset
+    y = point[1] * yfactor + yoffset
+
+    # flip the y coord so the origin is at the bottom
+    y = screen_range[1] - y
+
+    return (x,y)
 
 
-def draw_point(pair):
-    pygame.draw.circle(screen,(255,0,0),pair,2)
+def draw_point(point):
+    """draw a point in the screen"""
+    pygame.draw.circle(screen,(255,0,0),point,2)
 
 
+# just for testing purposes
 if __name__ == "__main__":
-
     init()
-    plot([(100,100),(200,100),(300,150)])
-
-
-
-
-
+    p
--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import unittest
 class TestPlot(unittest.TestCase):
 
     def test_origin(self):
-        """A point's y coordinates has to be transposed to the bottom of the screen if the coordinate origin is at 0,0"""
+        """A point's y coordinates have to be transposed to the bottom of the screen if the coordinate origin is at 0,0"""
         point=(0,0)
         self.assertEquals(
             plot.convert_point(point,xrange=(0,100),yrange=(0,100),screen_range=(400,400)),
-            (400,400))
+            (0,400))
 
     
                                
